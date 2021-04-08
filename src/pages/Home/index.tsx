b9148af62cc7d20b52d50c2545c24a962a1d01a2
IanaCris <ianasousati@gmail.com>
2021-04-08 15:08:20 -0300

    update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -30,7 +30,7 @@ const Home = (): JSX.Element => {
     const newSumAmount = {...sumAmount};
     newSumAmount[product.id] = product.amount;
    
-    return sumAmount; 
+    return newSumAmount; 
   }, {} as CartItemsAmount)
 
   useEffect(() => {
